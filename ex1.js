85d5675d2153ec0414d2f1bf37e7e6254ddef0a7
nikt01 <dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.com>
2023-11-27 21:33:14 +0100

    modified:   ex1.js

--- ex1.js
+++ ex1.js
@@ -1,13 +1,14 @@
- let  Godz = new Date().getHours();
+ var  Godz = new Date().getHours();
  let  Minuty = new Date().getMinutes();
    
 
  
  alert(Godz +":"+ Minuty);
-if(Godz=21){
+if (Godz=21) {
+    
     cyfra_hiszpanska = "veintiuno"
 
-    return alert = cyfra_hiszpanska
+ alert = "veintiuno"
 }
 
 
