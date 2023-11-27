b8411e168fd1ac33beb1cb1792f12860b5c8395c
nikt01 <dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.com>
2023-11-27 20:55:02 +0100

    modified:   ex1.js

--- ex1.js
+++ ex1.js
@@ -1,14 +1,5 @@
+ let  Teraz = Date().getHour; 
+ alert(Teraz);
 
 
-
- var data = new Date();
-alert('Date(27.11.2023): '+data);
-alert('Metoda getDate: '+data.getDate());
-alert('Metoda getDay: '+data.getDay());
-alert('Metoda getFullYear: '+data.getFullYear());
-alert('Metoda getHours: '+data.getHours());
-alert('Metoda getMilliseconds: '+data.getMilliseconds());
-alert('Metoda getMinutes: '+data.getMinutes());
-alert('Metoda getMonth: '+data.getMonth());
-alert('Metoda getSeconds: '+data.getSeconds());
-alert('Metoda getTime: '+data.getTime());
+ 
