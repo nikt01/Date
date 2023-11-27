843152b3e1c1cf85cf5103ac969bc146cdf62aa1
nikt01 <dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.comgit config --global user.email dobberlukasz@gmail.com>
2023-11-27 23:20:17 +0100

    modified:   ex2.js

---
+++ ex2.js
@@ -0,0 +1,12 @@
+function getWeeklyDay(year, month, day) {
+    return ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado","Domingo"][
+      new Date(year, month - 1, day).getDay()
+    ] || "Wrong Day Or Undefined";
+  }
+  
+  var year = 2023;
+  var month = 11; 
+  var day = 27;
+  var dayOfWeek = getWeeklyDay(year, month, day);
+  console.log("Day of the week: " + dayOfWeek);
+  
